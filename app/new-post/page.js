9473e8249263ba135f412d15f6bd08e5a796c8dd
Santosh Kumar <santoshku8287@gmail.com>
2025-06-02 21:04:43 +0530

    basic server side form actions

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,4 +1,21 @@
+import { storePost } from "@/lib/posts";
+
 export default function NewPostPage() {
+
+  async function createPost(formData){
+    "use server";
+    const title = formData.get('title');
+    const image = formData.get('image');
+    const content = formData.get('content');
+    storePost({
+      imageUrl: '',
+      title,
+      content,
+      userId: 1
+    })
+  }
+
+
   return (
     <>
       <h1>Create a new post</h1>
